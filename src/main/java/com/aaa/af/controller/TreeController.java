cc09a0360fc45a7519feb8d5ef1c687ea84cb0e8
youzhi110 <958573875@qq.com>
2018-12-21 16:39:55 +0800

    小明增加账户管理。

--- src/main/java/com/aaa/af/controller/TreeController.java
+++ src/main/java/com/aaa/af/controller/TreeController.java
@@ -3,14 +3,12 @@ package com.aaa.af.controller;
 import com.aaa.af.service.TreeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import java.util.List;
-import java.util.Map;
+
 
 /**
  * className:TreeController
--- src/main/java/com/aaa/af/dao/TreeDao.java
+++ src/main/java/com/aaa/af/dao/TreeDao.java
@@ -1,6 +1,7 @@
 package com.aaa.af.dao;
 
 import com.aaa.af.entity.Children;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -17,7 +18,7 @@ public interface TreeDao {
      * 查询树节点
      * @return
      */
-    @Select("select tr.* from tb_user u left join tb_role r on r.roleid=u.roleid left join tb_middle m on m.roleId =r.roleId left join tb_treememu tr on tr.treeId=m.treeId where u.name=#{username}")
+    @Select("select tr.* from tb_user u left join tb_role r on r.roleid=u.roleid left join tb_middle m on m.roleId =r.roleId left join tb_treememu tr on tr.treeId=m.treeId where u.name=#{param1}")
     List<Children> getListTree(String username);
     /**
      * 查询全部树节点
@@ -26,3 +27,4 @@ public interface TreeDao {
     @Select("select treeId,lable,treeUrl,treeParent from TB_TREEMEMU")
     List<Children> getListAllTree();
 }
+
